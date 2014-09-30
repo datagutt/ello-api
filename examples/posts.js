2ac795a8acbfa945644bddecad979a7b21cee801
Thomas Lekanger <datagutt@lekanger.no>
2014-09-30 20:44:00 +0200

    Improvements and WIP posting support

--- examples/posts.js
+++ examples/posts.js
@@ -3,7 +3,13 @@ var Ello = require('../index');
 var username = process.env.UN;
 var password = process.env.PW;
 Ello.login(username, password, function(err, ello){
-	ello.getUser('datagutt', function(err, user){
-		console.log(user);
-	});
+	if(err){
+		console.log(err);
+	}
+	//ello.getUser('datagutt', function(err, user){
+	//	console.log(user);
+	//});
+	ello.say('text', 'howdy', function(err, result){
+		console.log(err, result)
+	})
 });
